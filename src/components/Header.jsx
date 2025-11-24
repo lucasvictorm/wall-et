42d9a3996b22aff5b7b30d5e2dd94164a5d4662b
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2025-11-23 23:07:05 -0300

    atualização de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 
-import logo from '../assets/logo.png'
+import logo from '../assets/logo-wallet.png'
 
 function Header({children}){
     return(
-        <>
+        <div className='bg-linear-to-t from-green-900 to-green-700 p-6'>
+
             <div className="w-full p-2">
                 <img className='h-10' src={logo} alt="" />
 
@@ -13,7 +14,8 @@ function Header({children}){
                 {children}
 
             </div>
-        </>
+        </div>
+        
     )
 }
 
